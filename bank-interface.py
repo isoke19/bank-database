6c7313df9b0639bfbe7a62274c62465b942c4c29
zhoumandy <mzhou61@asu.edu>
2023-11-26 21:42:48 -0700

    basic sketelon code including sign up, login, and bank action processes

--- bank-interface.py
+++ bank-interface.py
@@ -1,35 +1,94 @@
-import subprocess
 import os
+import subprocess
 import psycopg2
 
-HOME = os.environ.get('HOME')
-subprocess.run(["pg_ctl", "-D", f"{HOME}/bankproj", "-o","'-k /tmp'", "start"], stdout=subprocess.DEVNULL)
-print("server started:")
+from InquirerPy import inquirer
 
-conn = psycopg2.connect(database = 'mandyzhou',
-                        user = 'mandyzhou',
-                        host = '/tmp',
-                        port = '8888')
+def main():
 
-cur = conn.cursor()
+    # environment variables
+    HOME = os.environ.get('HOME')
 
-cur.execute("SELECT * from Customer;")
-result = cur.fetchall()
-print(result)
+    # connects to postgresql server
+    subprocess.run(["pg_ctl", "-D", f"{HOME}/bankproj", "-o","'-k /tmp'", "start"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print("POSTGRESQL SERVER STARTED")
 
-print("---")
+    # to test, change 'database' and 'user' values:
+    # to find these values, log in to your postgresql and type '\conninfo'
+    # ex. 
+    # mandyzhou=# \conninfo
+    # You are connected to database "mandyzhou" as user "mandyzhou" via socket in "/tmp" at port "8888".
+    conn = psycopg2.connect(database = 'mandyzhou', user = 'mandyzhou', host = '/tmp', port = '8888')
 
-cur.execute("""
-INSERT INTO Customer (c_id, password, first_name, last_name, birthday)
-VALUES (9, '2004', 'Jasmine', 'Situ', '2004-06-18');
-""")
-cur.execute("SELECT * from Customer;")
-result = cur.fetchall()
-print(result)
+    cur = conn.cursor()
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
 
-subprocess.run(["pg_ctl", "-D", f"{HOME}/bankproj", "stop"], stdout=subprocess.DEVNULL)
-print("server stopped.")
+    # sign up / login
+    entry = inquirer.select(
+        message='Welcome to the ASU Bank! Select:',
+        choices=['Log In', 'Sign Up']
+    ).execute()
+
+    if entry == 'Log In':
+        customer = login()
+
+    if entry == 'Sign Up':
+        customer = sign_up()
+
+    # actions
+
+    action = 'Start'
+    while action != 'Log Out':
+        action = inquirer.select(
+            message='Select Action:',
+            choices=['Open a New Bank Account', 'Deposit', 'Withdraw', 'Send Money', 'Check Transaction History', 'Check Current Balance', 'Log Out']
+        ).execute()
+
+        if action == 'Open a New Bank Account':
+            print("Open a New Bank Account [action]")
+
+        if action == 'Deposit':
+            print('Deposit [action]')
+        
+        if action == 'Withdraw':
+            print('Withdraw [action]')
+
+        if action == 'Send Money':
+            print('Send Money [action]')
+
+        if action == 'Check Transaction History':
+            print('Check Transaction History [action]')
+
+        if action == 'Check Current Balance':
+            print('Check Current Balance [action]')
+    
+    print('Logging out...')
+
+    # quits connection to postgresql server
+    subprocess.run(["pg_ctl", "-D", f"{HOME}/bankproj", "stop"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print("POSTGRESQL SERVER STOPPED")
+
+def login():
+    '''
+    require : c_id (customer_id) and password
+    task    : find matching customer in database, else return error or sign up
+    return  : Customer object
+    '''
+    return 'customer_obj_placeholder_from_log_in'
+
+def sign_up():
+    '''
+    require : N/A
+    task    : collect user input for Customer attributes (first_name, last_name, birthday, password)
+              create c_id
+              create Customer object
+    return  : Customer object
+    '''
+    return 'customer_obj_placeholder_from_sign_up'
+
+if __name__ == "__main__":
+    main()
+    
 
